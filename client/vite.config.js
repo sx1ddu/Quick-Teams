da7738bbd4bc053b33b862ee2ec11b24a9dd3788
sx1ddu <ksiddharthaphule@gmail.com>
2026-07-07 00:01:00 +0530

    deploy: configure application for Vercel

--- client/vite.config.js
+++ client/vite.config.js
@@ -7,6 +7,7 @@ export default defineConfig({
   plugins: [react()
     , tailwindcss()
   ],
+  base : process.env.VITE_BASE_URL || '/Quick-Teams',
     server: {
     proxy: {
       "/api": "http://localhost:5000", 
